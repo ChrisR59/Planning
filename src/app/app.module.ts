68bd4b61b0d5e06ec938ee4958dba78760cb1aca
Zalendil <noctis_59@yahoo.fr>
2020-08-24 14:52:30 +0200

    Ajout component et service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DayComponent } from './day/day.component';
+import { ActivityComponent } from './activity/activity.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { DayListComponent } from './day-list/day-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DayComponent,
+    ActivityComponent,
+    HeaderComponent,
+    BodyComponent,
+    ActivityListComponent,
+    DayListComponent
   ],
   imports: [
     BrowserModule,
